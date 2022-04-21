a8e8f0edb70e249347c9871216e34e9d27ccf434
epk3 <ekrajewski3@protonmail.com>
2022-04-21 11:09:30 -0400

    finished

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const input = document.getElementById("search");
-let query = document.getElementById("search").value;
 let currentLat;
 let currentLon;
+
+
+var map = L.map('map').setView([39.03, -77.5], 12);
+var marker = L.marker([39.03, -77.5]).addTo(map);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZXBrMyIsImEiOiJjbDIzcWNuYWcwcXQxM29wN2F1Z2k5cjJkIn0.GAc85N093LVVq9qu1JvW1g'
+}).addTo(map);
+
+const updateMap = (update_map) => {
+  map.setView(update_map, 12);
+  L.marker(update_map).addTo(map);
+  console.log(update_map)
+}
+
 
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
@@ -9,13 +28,18 @@ input.addEventListener("keyup", function(event) {
   }
 }); 
 
-fetch(`http://ip-api.com/json/${query}?fields=status,region,city,zip,lat,lon,timezone,isp,query`)
-.then(response => response.json())
-.then(data => {
+const searchInput = () => {
+  let query = document.getElementById("search").value;
+  fetch(`http://ip-api.com/json/${query}?fields=status,region,city,zip,lat,lon,timezone,isp,query`)
+  .then(response => response.json())
+  .then(data => {
   currentIpAddress.innerHTML = data.query
   currentLocation.innerHTML = data.city + ', ' + data.region + ' ' + data.zip
-  currentTimezone.innerHTML = data.currentTimezone
   currentISP.innerHTML = data.isp
-  currentLat = data.lat
-  currentLon = data.lon
+  currentTimezone.innerHTML = data.timezone.replace("_", " ");
+  
+  updateMap([parseFloat(data.lat), parseFloat(data.lon)]);
 });
+};
+
+let query = searchInput().toString();
